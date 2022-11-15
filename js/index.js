9c878eaa4e9c64ab63e145852422da6d926cfcb1
crappe91 <crappe1991@gamil.com>
2022-11-15 02:36:12 +0100

    mapa estadio

---
+++ js/index.js
@@ -0,0 +1,80 @@
+
+
+  $(document).ready(function(){
+    
+    //FONDO
+    
+    $("#F2").click(function(){
+    $("#nombreGrada").empty();  
+    $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp FONDO SEGUNDO ANFITEATRO</b>.");
+    });
+  
+    $("#F1").click(function(){
+        $("#nombreGrada").empty(); 
+        $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp FONDO PRIMER ANFITEATRO</b>.");
+      });
+
+    $("#FB").click(function(){
+        $("#nombreGrada").empty(); 
+        $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp FONDO TRIBUNA BAJA</b>.");
+      });
+        
+    //PREFERENCIA
+    
+    $("#P2").click(function(){
+        $("#nombreGrada").empty(); 
+        $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp PREFERENCIA SEGUNDO ANFITEATRO</b>.");
+      });
+    
+      $("#P1").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp PREFERENCIA PRIMER ANFITEATRO</b>.");
+        });
+  
+      $("#PB").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp PREFERENCIA TRIBUNA BAJA</b>.");
+        });
+          
+    //GOL NORTE
+    
+    $("#N2").click(function(){
+        $("#nombreGrada").empty(); 
+        $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL NORTE SEGUNDO ANFITEATRO</b>.");
+      });
+    
+      $("#N1").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL NORTE PRIMER ANFITEATRO</b>.");
+        });
+  
+      $("#NB").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL NORTE TRIBUNA BAJA</b>.");
+        });
+          
+    //GOL SUR 
+    
+    $("#S2").click(function(){
+        $("#nombreGrada").empty(); 
+        $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL SUR SEGUNDO ANFITEATRO</b>.");
+      });
+    
+      $("#S1").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL SUR PRIMER ANFITEATRO</b>.");
+        });
+  
+      $("#SB").click(function(){
+        $("#nombreGrada").empty(); 
+          $("p").append("<b>&nbsp&nbspHa seleccionado:<br>&nbsp&nbsp GOL SUR TRIBUNA BAJA</b>.");
+        });
+
+      
+
+      
+
+
+
+
+  });
